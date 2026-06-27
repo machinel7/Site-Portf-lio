3c2940047da89b1a15740b7ae9b42f923aa5f013
João <joobarbosa04@gmail.com>
2026-06-26 23:14:39 -0300

    atualizaçao projetos

--- src/data/projects.js
+++ src/data/projects.js
@@ -9,6 +9,15 @@ const projects = [
     github: "https://github.com/FVSSANTOS/TaskIA-BackEnd",
   },
 
+    {
+    id: 2,
+    name: "Splitwise++",
+    description:
+      "Plataforma de divisão de despesas em grupo (tipo Splitwise melhorado), algoritmo de simplificação de dívidas (quem deve pra quem, minimizando transações, gráficos de gastos, exportação de relatórios em PDF.",
+    period: "Jun 2026",
+    technologies: ["React + Next.js + TypeScript", "Node.js + NestJS", "PostgreSQL", "JWT", "WebSockets"],
+    github: "https://github.com/machinel7/splitwiseplus",
+    },
 ];
 
 export default projects;
